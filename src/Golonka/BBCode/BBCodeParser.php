7ea68e910addc8a18443f58a176ca459af407168
Yuri Moens <yuri.moens@gmail.com>
2014-03-25 10:43:49 +0100

    Added support for old list notations.

--- src/Golonka/BBCode/BBCodeParser.php
+++ src/Golonka/BBCode/BBCodeParser.php
@@ -59,8 +59,16 @@ class BBCodeParser {
             'pattern' => '/\[list=a\](.*?)\[\/list\]/s', 
             'replace' => '<ol type="a">$1</ol>'
         ),
+        'orderedListDeprecated' => array(
+            'pattern' => '/\[ol\](.*?)\[\/ol\]/s', 
+            'replace' => '<ol>$1</ol>'
+        ),
         'unorderedList' => array(
             'pattern' => '/\[list\](.*?)\[\/list\]/s', 
+            'replace' => '<ul>$1</ul>'
+        ),
+        'unorderedListDeprecated' => array(
+            'pattern' => '/\[ul\](.*?)\[\/ul\]/s', 
             'replace' => '<ul>$1</ul>'
         ),
         'listItem' => array(
